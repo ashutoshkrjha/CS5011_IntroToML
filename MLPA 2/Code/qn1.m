ea7de285c5d4ef64e948f8a1267ff045eacd7339
Ashutosh <ashutoshkrjha@github.com>
2016-11-24 12:28:19 +0530

    Added final submissions of MLPA 2 and 3

--- MLPA 2/Code/qn1.m
+++ MLPA 2/Code/qn1.m
@@ -3,17 +3,25 @@ Xtest = mmread('Test_features');
 Ytest = mmread('Test_labels');
 Ytrain = mmread('Train_labels');
 
-% Ytest = repmat(Ytest,1,4);
-% Ytest((find(Ytest(:,1)==1)),:)=repmat([1 0 0 0],size(Ytest((find(Ytest(:,1)==1)),:),1),1);
-% Ytest((find(Ytest(:,1)==2)),:)=repmat([0 1 0 0],size(Ytest((find(Ytest(:,1)==2)),:),1),1);
-% Ytest((find(Ytest(:,1)==3)),:)=repmat([0 0 1 0],size(Ytest((find(Ytest(:,1)==3)),:),1),1);
-% Ytest((find(Ytest(:,1)==4)),:)=repmat([0 0 0 1],size(Ytest((find(Ytest(:,1)==4)),:),1),1);
-% 
-% Ytrain = repmat(Ytrain,1,4);
-% Ytrain((find(Ytrain(:,1)==1)),:)=repmat([1 0 0 0],size(Ytrain((find(Ytrain(:,1)==1)),:),1),1);
-% Ytrain((find(Ytrain(:,1)==2)),:)=repmat([0 1 0 0],size(Ytrain((find(Ytrain(:,1)==2)),:),1),1);
-% Ytrain((find(Ytrain(:,1)==3)),:)=repmat([0 0 1 0],size(Ytrain((find(Ytrain(:,1)==3)),:),1),1);
-% Ytrain((find(Ytrain(:,1)==4)),:)=repmat([0 0 0 1],size(Ytrain((find(Ytrain(:,1)==4)),:),1),1);
+m = size(Xtrain, 1);
+Xtrainmean = mean(Xtrain);
+Xtestmean = mean(Xtest);
+Xtrainstd = std(Xtrain);
+Xteststd = std(Xtest);
 
-model = svmtrain(Ytrain, Xtrain);
-[predicted_label, accuracy, prob_estimates] = svmpredict(Ytest, Xtest, model);
+Xtrainmean = repmat(Xtrainmean,m,1);
+Xtestmean = repmat(Xtestmean,size(Xtest,1),1);
+Xtrainstd = repmat(Xtrainstd,m,1);
+Xteststd = repmat(Xteststd,size(Xtest,1),1);
+
+Xtrain = (Xtrain-Xtrainmean)./(Xtrainstd);
+Xtest = (Xtest-Xtestmean)./(Xteststd);
+
+%Change the model parameters here to get the various answers
+model1 = svmtrain(Ytrain, Xtrain,'-t 0 -c 0.1');
+model2 = svmtrain(Ytrain, Xtrain,'-t 1 -c 3 -d 1');
+model3 = svmtrain(Ytrain, Xtrain,'-t 2 -c 1 -g 0.01');
+model4 = svmtrain(Ytrain, Xtrain,'-t 3 -c 0.1 -g 0.01 -r 0');
+
+save('ME14B148','model1','model2','model3','model4');
+% [predicted_label, accuracy, prob_estimates] = svmpredict(Ytest, Xtest, model);
